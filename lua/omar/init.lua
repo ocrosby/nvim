77c5b591e3f4de964b5448e8c50bf616a8ea862f
Omar Crosby <omar.crosby@weather.com>
2025-04-17 14:02:59 -0400

    chore: rename set.lua to options.lua

--- lua/omar/init.lua
+++ lua/omar/init.lua
@@ -2,7 +2,7 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require("omar.set") -- Load settings
+require("omar.options") -- Load settings
 require("omar.keymaps") -- Load keymaps
 require("omar.autocmds") -- Load autocmds early
 require("omar.config.lazy")  -- Load lazy.nvim
