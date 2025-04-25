aeda647ea74fd931228fe31c75565a1444adc63c
Omar Crosby <omar.crosby@weather.com>
2025-04-25 15:03:29 -0400

    chore: add pytest runner telescope extension

--- lua/omar/config/telescope.lua
+++ lua/omar/config/telescope.lua
@@ -23,11 +23,20 @@ telescope.setup({
     },
 })
 
+-- Load the pytest_runner extension
+pcall(require("telescope").load_extension, "pytest_runner")
+
 -- Telescope Keymaps
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})     -- Ctrl + P to find git files
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {}) -- Ctrl + P to find files
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {}) -- Ctrl + Shift + F to search in files
+
+
+-- Custom Pytest Runner shortcut
+vim.keymap.set('n', '<leader>tp', function()
+    require('telescope').extensions.pytest_runner()
+end, { desc = "Run pytest preprocessor picker" }
 
 -- Load Telescope extensions if any
 -- telescope.load_extension("fzf")
---
+++ lua/telescope/_extensions/pytest_runner.lua
@@ -0,0 +1,159 @@
+-- File: lua/telescope/_extensions/pytest_runner.lua
+
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local conf = require("telescope.config").values
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
+local Path = require("plenary.path")
+local yaml = require("yaml")  -- Assume you have a Lua YAML parser available
+
+local M = {}
+
+-- Default presets
+local default_environments = { "qa", "prod", "legacy" }
+local default_regions = { "auto", "use1", "usw2", "euw1", "apse1" }
+
+--- Read ingress-mapping.yaml if it exists.
+local function read_ingress_mapping()
+  local ingress_file = Path:new("ingress-mapping.yaml")
+  if not ingress_file:exists() then
+    return nil
+  end
+
+  local content = ingress_file:read()
+  local parsed = yaml.load(content)
+  return parsed
+end
+
+--- Get list of environments dynamically or fall back
+local function get_environments()
+  local ingress = read_ingress_mapping()
+  if ingress and ingress.environments then
+    local envs = {}
+    for _, env in ipairs(ingress.environments) do
+      table.insert(envs, env.name)
+    end
+    return envs
+  end
+  return default_environments
+end
+
+--- Get list of regions for a selected environment dynamically or fallback
+local function get_regions_for_environment(selected_env)
+  local ingress = read_ingress_mapping()
+  if ingress and ingress.environments then
+    for _, env in ipairs(ingress.environments) do
+      if env.name == selected_env then
+        local regions = {}
+        for _, region in ipairs(env.regions or {}) do
+          table.insert(regions, region.name)
+        end
+        return regions
+      end
+    end
+  end
+  return default_regions
+end
+
+--- Read pytest markers from pytest.ini
+local function read_markers_from_ini()
+  local markers_set = {}
+  local ini = Path:new("pytest.ini")
+
+  if ini:exists() then
+    for _, line in ipairs(ini:readlines()) do
+      local opts = line:match("addopts.*-m%s+\"(.-)\"")
+      if opts then
+        for marker in opts:gmatch("[^%s]+") do
+          markers_set[marker] = true
+        end
+      end
+    end
+  end
+
+  local markers = vim.tbl_keys(markers_set)
+  table.sort(markers)
+  return markers
+end
+
+local function select_markers(env, region)
+  local markers = read_markers_from_ini()
+
+  pickers.new({}, {
+    prompt_title = "Select Markers",
+    finder = finders.new_table {
+      results = markers,
+    },
+    sorter = conf.generic_sorter({}),
+    attach_mappings = function(prompt_bufnr, _)
+      actions.select_default:replace(function()
+        local selections = {}
+        for _, entry in ipairs(action_state.get_selected_entries()) do
+          table.insert(selections, entry.value)
+        end
+        actions.close(prompt_bufnr)
+
+        local command = string.format('python3 preprocessor.py -e %s -r %s', env, region)
+
+        if #selections > 0 then
+          local marker_str = table.concat(selections, " and ")
+          marker_str = '"' .. marker_str .. '"'
+          command = command .. string.format(' -m %s', marker_str)
+        end
+
+        vim.notify("Generated command:\n" .. command, vim.log.levels.INFO)
+        vim.fn.setreg('+', command)  -- Copy command to clipboard
+      end)
+
+      return true
+    end
+  }):find()
+end
+
+local function select_region(env)
+  local regions = get_regions_for_environment(env)
+
+  pickers.new({}, {
+    prompt_title = "Select Region",
+    finder = finders.new_table {
+      results = regions,
+    },
+    sorter = conf.generic_sorter({}),
+    attach_mappings = function(prompt_bufnr, _)
+      actions.select_default:replace(function()
+        local selection = action_state.get_selected_entry()
+        actions.close(prompt_bufnr)
+        select_markers(env, selection.value)
+      end)
+      return true
+    end
+  }):find()
+end
+
+function M.run_pytest_picker()
+  local envs = get_environments()
+
+  pickers.new({}, {
+    prompt_title = "Select Environment",
+    finder = finders.new_table {
+      results = envs,
+    },
+    sorter = conf.generic_sorter({}),
+    attach_mappings = function(prompt_bufnr, _)
+      actions.select_default:replace(function()
+        local selection = action_state.get_selected_entry()
+        actions.close(prompt_bufnr)
+        select_region(selection.value)
+      end)
+      return true
+    end
+  }):find()
+end
+
+return require("telescope").register_extension({
+  exports = {
+    pytest_runner = M.run_pytest_picker,
+  },
+})
+
